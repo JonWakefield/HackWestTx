ce350ee452bab52a6a4bd70121f0647c5af41130
Jon Wakefield <67289517+JonWakefield@users.noreply.github.com>
2023-02-26 08:32:17 -0600

    renamed:    face.py -> face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,41 +1,43 @@
 import cv2
 
-# Load the cascade
+# Use Haarcascade classifier for facial recognition
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-
+# If unable to open / locate file, end program
 if face_cascade.empty():
     raise IOError('Unable to load the face cascade classifier xml file')
+
 
 # Open webcam capture
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Read the frame
+
+    # Read in the image
     ret, img = cap.read()
 
-    # Convert to grayscale
+    # Convert img to grayscale to allow for face classification
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces
+    
+    # use face_cascade to look for faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    # Draw rectangles around the faces
+
+    # If theres a face, draw a square around it
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        # IF WE ENTER THIS PART OF THE CODE IT MEANS A FACE HAS BEEN DETECTED:
-        #print("face")
 
-    # Show the frame
+
+    # Display the frame
     cv2.imshow('Face detection', img)
+
 
     # Stop if 'q' key is pressed
     if cv2.waitKey(1) == ord('q'):
         break
 
 # Release the capture and close the window
-
-
 cap.release()
 cv2.destroyAllWindows()
